98b002af63fc77eb876b2e22923d3a05204e300f
Kris Leech <kris.leech@gmail.com>
2014-11-24 20:28:25 +0000

    add correct URL for routemaster

--- app/models/routemaster_listener.rb
+++ app/models/routemaster_listener.rb
@@ -3,15 +3,34 @@ require 'routemaster/client'
 # Handler for events which need to be propergated to Routemaster
 
 class RoutemasterListener
-  # will not work until setup IP's for VM's to communicate
   def on_vote_cast(song_id)
-    client.created('songs', "http://localhost:9000/songs/#{song_id}") # this needs to be HTTPS
+    publish_created('songs', song_url(song_id))
   end
 
   private
 
-  # TODO: get URL and UUID from env
+  def publish_created(topic, url)
+    Rails.logger.debug("-------------------------> create #{topic} #{url} #{routemaster_url} #{routemaster_uuid}")
+    client.created(topic, url)
+  end
+
   def client
-    @client ||= Routemaster::Client.new(url: 'https://localhost:4434', uuid: 'demo')
+    @client ||= Routemaster::Client.new(url: routemaster_url, uuid: routemaster_uuid)
+  end
+
+  def song_url(song_id)
+    "#{jukebox_url}/songs/#{song_id}"
+  end
+
+  def jukebox_url
+    "https://192.168.1.1:#{ENV.fetch('JUKEBOX_PORT')}"
+  end
+
+  def routemaster_url
+    ENV.fetch('ROUTEMASTER_URL')
+  end
+
+  def routemaster_uuid
+    ENV.fetch('ROUTEMASTER_UUID')
   end
 end
